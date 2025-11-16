700327337f24ef50516b3faaae13d76be1c1f0f0
Gere Áron <gere.aron96@gmail.com>
2025-11-16 16:46:56 +0100

    add to urm

--- Altruist/Core/Framework/Persistence/UORM/UORM.cs
+++ Altruist/Core/Framework/Persistence/UORM/UORM.cs
@@ -58,3 +58,17 @@ public class VaultColumnAttribute : Attribute
     public string? Name { get; }
     public VaultColumnAttribute(string? name = null) => Name = name;
 }
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = true)]
+public sealed class VaultForeignKeyAttribute : Attribute
+{
+    public Type PrincipalType { get; }
+    public string PrincipalPropertyName { get; }
+
+    public VaultForeignKeyAttribute(Type principalType, string principalPropertyName)
+    {
+        PrincipalType = principalType ?? throw new ArgumentNullException(nameof(principalType));
+        PrincipalPropertyName = principalPropertyName ?? throw new ArgumentNullException(nameof(principalPropertyName));
+    }
+}
+
