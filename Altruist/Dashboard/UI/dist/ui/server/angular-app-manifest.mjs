ef4fe658bcfd6c094eeb9920668408148ba111e7
Gere Áron <gere.aron96@gmail.com>
2025-12-08 19:56:49 +0100

    update session

--- Altruist/Dashboard/UI/dist/ui/server/angular-app-manifest.mjs
+++ Altruist/Dashboard/UI/dist/ui/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 811, hash: 'f7e17b0d65ab385ecd211623eaa3f5e8d0f470a1cc69b8203d8d89a8c492e5b2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1327, hash: '79d2c4ac5c06ae29a28d188d31412440119deef75fe51f81ec31692c62102d29', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 811, hash: '3108b59a6937de355f5af1578c06b261261f2db36611b20f414b1452256c0284', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1327, hash: 'e52dddd7dfa3a2fba4f10d47ceb2f27d20385d0e5d333a19e47999b27901e1e3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
