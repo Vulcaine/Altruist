e10e0519ad242d002998ee3e1a06036c3055a7f3
Gere Áron <gere.aron96@gmail.com>
2025-12-08 17:52:09 +0100

    fix conditions

--- Altruist/Postgres/Core/PgServiceFactory.cs
+++ Altruist/Postgres/Core/PgServiceFactory.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Logging;
 namespace Altruist.Persistence.Postgres;
 
 [Service(typeof(IServiceFactory), ServiceLifetime.Singleton)]
-[ConditionalOnConfig("altruist:persistance:database:provider", havingValue: "postgres")]
+[ConditionalOnConfig("altruist:persistence:database:provider", havingValue: "postgres")]
 public sealed class PostgresServiceFactory : IServiceFactory
 {
     public bool CanCreate(Type serviceType)
