5b6d405697dbcb65f939d00e2530235c648d5a2b
gerearon <gere.aron96@gmail.com>
2025-12-11 17:43:47 +0100

    fix ui

--- Altruist/Dashboard/UI/dist/ui/server/angular-app-manifest.mjs
+++ Altruist/Dashboard/UI/dist/ui/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 1188, hash: '8ef4a1162e440f9fb86b79513f0f97ca4e29ef81e66a3c1118f35c9165f62873', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1327, hash: 'fe980ad38048a5b829e8d5d68510476177ee4335a449652b6825d8019158f265', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1188, hash: '8d97c3bb7495db197493d5d839c39dcc761a21a8a439dfdd8c0409d580c78017', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1327, hash: '3695a59de28f44a150a199fac0b7e3271c3be70f39eb76911edd04976aad98dc', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-A6JBY4VQ.css': {size: 4212, hash: 'F45Ls6H/xEk', text: () => import('./assets-chunks/styles-A6JBY4VQ_css.mjs').then(m => m.default)}
   },
 };
